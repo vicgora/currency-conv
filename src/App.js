0b21bbe2a38981ecf7164fa1dfa2f9d98f874d31
vicgora <vicgora@gmail.com>
2024-11-26 17:04:58 +0100

    Primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useRef, useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [resultadoUSD, setResultadoUSD] = useState(0);
+    const [resultadoEUR, setResultadoEUR] = useState(0);
+    const [tasaCambioUSD, setTasaCambioUSD] = useState(0); // Valor por defecto
+    const [tasaCambioEUR, setTasaCambioEUR] = useState(0); // Valor por defecto
+    const inputEuro = useRef(null);
+    const inputDolar = useRef(null);
+
+    useEffect(() => {
+        fetch(
+            'https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/eur.json'
+        )
+            .then((response) => response.json())
+            .then((data) => {
+                setTasaCambioUSD(data.eur.usd);
+            })
+            .catch((error) =>
+                console.error('Error al obtener la tasa de cambio:', error)
+            );
+    }, []);
+
+    useEffect(() => {
+        fetch(
+            'https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/usd.json'
+        )
+            .then((response) => response.json())
+            .then((data) => {
+                setTasaCambioEUR(data.usd.eur);
+            })
+            .catch((error) =>
+                console.error('Error al obtener la tasa de cambio:', error)
+            );
+    }, []);
+
+    const convertirUSD = () => {
+        const cantidad = inputEuro.current.value;
+        setResultadoUSD(cantidad * tasaCambioUSD);
+        console.log(cantidad * tasaCambioUSD);
+    };
+
+    const convertirEUR = () => {
+        const cantidad = inputDolar.current.value;
+        setResultadoEUR(cantidad * tasaCambioEUR);
+        console.log(cantidad * tasaCambioEUR);
+    };
+
+    return (
+        <div id="main">
+            <div id="eur-usd">
+                <div id="titulo">
+                    <h2>Conversor Euro-Dólar</h2>
+                </div>
+                <div id="euro">
+                    <input type="text" size={4} ref={inputEuro} />€
+                </div>
+                <div id="convertir">
+                    <button onClick={convertirUSD}>Convertir</button>
+                </div>
+                <div id="resultado">
+                    <h3>{resultadoUSD} $</h3>
+                </div>
+            </div>
+            <div id="usd-eur">
+                <div id="titulo">
+                    <h2>Conversor Dólar-Euro</h2>
+                </div>
+                <div id="dolar">
+                    <input type="text" size={4} ref={inputDolar} />$
+                </div>
+                <div id="convertir">
+                    <button onClick={convertirEUR}>Convertir</button>
+                </div>
+                <div id="resultado">
+                    <h3>{resultadoEUR} €</h3>
+                </div>
+            </div>
+            <footer id="pie">
+                <a href="https://github.com/fawazahmed0/exchange-api?tab=readme-ov-file">
+                    API de tasa de cambio
+                </a>
+            </footer>
+        </div>
+    );
 }
 
 export default App;
